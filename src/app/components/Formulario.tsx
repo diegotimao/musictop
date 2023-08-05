c503be337e6cd935e6360d0452d30e5e3c8ee0f0
Diego <diegotimao1104@gmail.com>
2023-08-05 19:40:00 -0300

    feat: Criando o componente de Formulario

---
+++ src/app/components/Formulario.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React, { useContext, useState } from 'react';
+import FormLogin from './FormLogin';
+import FormRegister from './FormRegister';
+import ButtoSocials from './ButtoSocials';
+import logoGoole from '../../../public/logogoogle.png';
+import logoGithub from '../../../public/logogithub.png';
+import { AuthContext } from '@/context/auth';
+
+export default function Formulario({ pathName }: { pathName: string}) {
+  return (
+    <section className='bg-white w-full p-10 sm:max-w-xl md:max-w-2xl
+        rounded-lg flex flex-col gap-8 border-spacing-2 border-gray-800'>
+      <div className='flex flex-col gap-3 pt-2'>
+        <div className='flex flex-col gap-3'>
+          <span className='text-xl font-semibold text-gray-500'>Seja bem-vindo(a)</span>
+          <h1 className='font-semibold text-4xl'>Faça seu {pathName === '/login' ? 'login' : 'cadastro'}</h1>
+          <span className='text-base'>Entre com seus dados e curta bons momentos escutando suas músicas e artistas favoritos.</span>
+        </div>
+      </div>
+      {pathName === '/login' ? <FormLogin /> : <FormRegister />}
+      <div className='flex flex-col sm:flex-row gap-2 '>
+          <ButtoSocials title='Google' url={logoGoole} />
+          <ButtoSocials title='Github' url={logoGithub} />
+      </div>
+      <div className='flex justify-center p-2'>
+        <span>Não tem uma conta? Registre-se</span>
+      </div>    
+    </section>
+  )
+}
