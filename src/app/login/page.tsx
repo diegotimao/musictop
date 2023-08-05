132a51e7ab889765ad3b3f42549ebdba2fa627b3
Diego <diegotimao1104@gmail.com>
2023-08-05 19:39:24 -0300

    feat: Criando a página de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+import Formulario from '../components/Formulario';
+import { usePathname } from 'next/navigation';
+
+export default function Login() {
+  const router = usePathname()
+
+  return (
+    <main className="flex min-h-screen p-5 flex-col items-center justify-center bg-gray-200 bg-[url('../../public/banner.jpg')]">
+      <Formulario pathName={router}/>
+    </main>
+  )
+}
