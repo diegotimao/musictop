e0fcb593ff6d2bbfff5d093b07da839a92c2454b
Diego <diegotimao1104@gmail.com>
2023-08-05 21:31:01 -0300

    refactor: Refatorando o componente Formulario

--- src/app/components/Formulario.tsx
+++ src/app/components/Formulario.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import FormLogin from './FormLogin';
 import FormRegister from './FormRegister';
 import ButtoSocials from './ButtoSocials';
