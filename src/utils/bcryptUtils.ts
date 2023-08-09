b05dcfcd7b5d1051f4cc50dfdbc525d73573319f
Diego <diegotimao1104@gmail.com>
2023-08-09 19:38:03 -0300

    feat: Criando a função que compara o hash da senha

--- src/utils/bcryptUtils.ts
+++ src/utils/bcryptUtils.ts
@@ -9,3 +9,14 @@ const hashPassword = async (password: string) => {
     throw new Error('Erro ao gerar o hash da senha:' +  (error as Error).message);
   }
 }
+
+const comparePassword = async (password: string, hash: string) => {
+  try {
+    const isMatch = await bcrypt.compare(password, hash);
+    return isMatch;
+  } catch (error) {
+    throw new Error('Erro ao comparar senha: ' + (error as Error).message)
+  }
+}
+
+export { hashPassword, comparePassword }
