94d0bb3bccfc2eaa966c84d30130841bee1159da
Diego <diegotimao1104@gmail.com>
2023-08-09 19:38:56 -0300

    feat: Passando o pathName através de props

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,11 +4,11 @@ import Formulario from '../components/Formulario';
 import { usePathname } from 'next/navigation';
 
 export default function Login() {
-  const router = usePathname()
+  const router = usePathname();
 
   return (
     <main className="flex min-h-screen p-5 flex-col items-center justify-center bg-gray-200 bg-[url('../../public/banner.jpg')]">
-      <Formulario pathName={router}/>
+      <Formulario pathName={router} />
     </main>
   )
 }
